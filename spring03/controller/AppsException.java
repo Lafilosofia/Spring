34e042520a0f717e6931e17239f6284726c2264c
Lihaozhe <55686617+Lafilosofia@users.noreply.github.com>
2020-12-13 10:45:17 +0800

    Add files via upload

---
+++ spring03/controller/AppsException.java
@@ -0,0 +1,30 @@
+package com.lhz.login.controller;
+
+public class AppsException extends RuntimeException {
+
+	public AppsException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public AppsException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AppsException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AppsException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AppsException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
